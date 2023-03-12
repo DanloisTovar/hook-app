c0e029d4d2ccf15f78e5807fbc6089e03d0f5a29
Danlois Tovar <danlois.tovar@gmail.com>
2023-03-11 23:13:30 -0300

    practices customHook "useFetch.

--- src/components/MultipleCustomHooks.jsx
+++ src/components/MultipleCustomHooks.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useFetch } from '../hooks/useFetch';
 
 export const MultipleCustomHooks = () => {
+    const { data, isLoading, hasError } = useFetch(
+        'https://api.breakingbadquotes.xyz/v1/quotes/5 ',
+    );
+
+    console.log({ data, isLoading, hasError });
     return (
         <div>
             <h1>BreakingBad Quotes</h1>
