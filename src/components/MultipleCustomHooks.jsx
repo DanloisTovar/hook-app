e8ec52042172694e48bdd1cc8d70327193bab126
Danlois Tovar <danlois.tovar@gmail.com>
2023-03-13 21:29:15 -0300

    practices usefetch + useCounter end.

--- src/components/MultipleCustomHooks.jsx
+++ src/components/MultipleCustomHooks.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { useFetch } from '../hooks/useFetch';
+/* import { useFetch } from '../hooks/useFetch'; */
+import { useFetch, useCounter } from '../hooks';
 
 export const MultipleCustomHooks = () => {
+    const { counter, incrementar, decrementar, reset } = useCounter(1);
+
     const { data, isLoading, hasError } = useFetch(
-        ' https://api.breakingbadquotes.xyz/v1/quotes/5',
+        `https://api.breakingbadquotes.xyz/v1/quotes/${counter}`,
     );
     const { author, quote } = !!data && data[0];
+
     return (
         <div>
             {isLoading ? (
@@ -26,7 +30,13 @@ export const MultipleCustomHooks = () => {
                         </footer>
                     </div>
 
-                    <button className="btn btn-primary">Siguiente</button>
+                    <button
+                        disabled={isLoading}
+                        className="btn btn-primary"
+                        onClick={() => incrementar()}
+                    >
+                        Siguiente
+                    </button>
                 </>
             )}
         </div>
