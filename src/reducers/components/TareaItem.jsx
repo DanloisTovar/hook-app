2f942d057475689101b0b98174aab59ee569874c
Danlois Tovar <danlois.tovar@gmail.com>
2023-03-26 15:49:53 -0300

    Practices useReducer toggle ready.

--- src/reducers/components/TareaItem.jsx
+++ src/reducers/components/TareaItem.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
-export const TareaItem = ({ id, description, OnDeleteTarea }) => {
+export const TareaItem = ({
+    id,
+    description,
+    OnDeleteTarea,
+    onToggleTarea,
+}) => {
     return (
         <>
             <li className="list-group-item d-flex justify-content-between">
-                <span className="align-self-center">{description}</span>
+                <span
+                    className="align-self-center"
+                    onClick={() => onToggleTarea(id)}
+                >
+                    {description}
+                </span>
                 <button
                     className="btn btn-danger"
                     onClick={() => OnDeleteTarea(id)}
