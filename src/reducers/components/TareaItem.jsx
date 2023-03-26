3cb7bf6c9f3522fb3cfd7b8fca9ed7a12d28bf1c
Danlois Tovar <danlois.tovar@gmail.com>
2023-03-26 16:24:40 -0300

    add tachado to tareas in reducer practices.

--- src/reducers/components/TareaItem.jsx
+++ src/reducers/components/TareaItem.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export const TareaItem = ({
     id,
+    done,
     description,
     OnDeleteTarea,
     onToggleTarea,
@@ -10,7 +11,9 @@ export const TareaItem = ({
         <>
             <li className="list-group-item d-flex justify-content-between">
                 <span
-                    className="align-self-center"
+                    className={`align-self-center ${
+                        done && 'text-decoration-line-through'
+                    }`}
                     onClick={() => onToggleTarea(id)}
                 >
                     {description}
